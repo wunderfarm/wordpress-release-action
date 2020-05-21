855014b87502c12062937d6dd612fb1f16a41a1d
Benedikt Kofler <benedikt.kofler@gmail.com>
2020-05-21 16:50:12 +0200

    add commit message if no release name available

--- index.js
+++ index.js
@@ -22,9 +22,15 @@ try {
     let githubRef = github.context.ref
     let commitSha = github.context.sha
     let eventPayload = github.context.payload
-    console.log(eventPayload)
-    let releaseName = github.event.release.name
-    let releaseBody = github.event.release.body
+    let message = ''
+    if (typeof(eventPayload.release) !== 'undefined') {
+        message = 'Release title: ' + releaseName + '\n' + 'Release Body: ' + releaseBody + '\n'
+    } else if (typeof(eventPayload.commits) !== 'undefined') {
+        message = 'Commit: ' + eventPayload.commits[0].message
+    }
+    message = message + 'Ref: ' + githubRef + ' (' + commitSha.substring(0,7) + ')'
+    let releaseName = eventPayload.release.name
+    let releaseBody = eventPayload.release.body
     console.log(execSync('composer validate').toString())
     console.log(execSync(`composer install --prefer-dist --no-progress --no-suggest`).toString())
     console.log(execSync(`composer update johnpbloch/wordpress wunderfarm/* --with-dependencies`).toString())
@@ -63,7 +69,7 @@ try {
         },
         StackId: awsOpsworksStackId,
         AppId: awsOpsworksAppId,
-        Comment: 'Release title: ' + releaseName + '\n' + 'Release Body: ' + releaseBody + '\n' + 'Ref: ' + githubRef + '\n' + 'Commit: ' + commitSha + ' (' + commitSha.substring(0,7) + ')'
+        Comment: message
     }
     opsworks.createDeployment(opsworksParams, function (err, data) {
         if (err) {
