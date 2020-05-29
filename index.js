485c89661fc51e48f874f8334832e734bad49327
Benedikt Kofler <benedikt.kofler@gmail.com>
2020-05-29 16:21:44 +0200

    add deployApp function

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ const awsRegion = core.getInput('aws-region')
 const awsOpsworksStackId = core.getInput('aws-opsworks-stack-id')
 const awsRdsDbArn = core.getInput('aws-rds-arn')
 
-let awsOpsworksAppId;
 AWS.config = new AWS.Config()
 AWS.config.accessKeyId = awsAccessKeyId
 AWS.config.secretAccessKey = awsSecretAccessKey
@@ -47,6 +46,72 @@ async function getAwsOpsworksApp() {
             }
         })
     })
+}
+
+function deployApp(awsOpsworksAppId) {
+    if (awsOpsworksAppId) {
+        let githubRef = github.context.ref
+        let commitSha = github.context.sha
+        let eventPayload = github.context.payload
+        let message = ''
+        if (typeof (eventPayload.release) !== 'undefined') {
+            message = 'Release: ' + eventPayload.release.name + ' - "' + eventPayload.release.body + '" '
+        } else if (typeof (eventPayload.commits) !== 'undefined') {
+            message = 'Commit: "' + eventPayload.commits[0].message + '" '
+        }
+        message = message + 'Ref: ' + githubRef + ' (' + commitSha.substring(0, 7) + ')'
+        console.log(execSync('composer validate').toString())
+        console.log(execSync(`composer install --prefer-dist --no-progress --no-suggest`).toString())
+        console.log(execSync(`composer update johnpbloch/wordpress wunderfarm/* --with-dependencies`).toString())
+        console.log(execSync(`sudo npm install -g n`).toString())
+        console.log(execSync(`sudo n v10.15`).toString())
+        console.log(execSync(`npm ci`).toString())
+        console.log(execSync(`npm run build`).toString())
+        execSync(`mkdir dist`).toString()
+        execSync(`cp -R vendor dist`).toString()
+        execSync(`cp -R wp dist`).toString()
+        execSync(`cp -R wp-content dist`).toString()
+        execSync(`cp -R .htaccess dist`).toString()
+        execSync(`cp -R index.php dist`).toString()
+        execSync(`cp -R wp-config.* dist`).toString()
+        execSync(`cp -R robots.txt dist 2>/dev/null || :`).toString()
+        execSync(`zip -rq ${wfWebname} ./dist`).toString()
+
+        let filename = wfWebname + '.zip'
+        let file = fs.readFileSync(filename)
+
+        let s3params = {
+            Bucket: awsS3Bucket,
+            Tagging: 'client=' + wfClient + '',
+            Key: awsOpsworksStackId + '/' + wfWebname + '_' + awsOpsworksAppId + '/' + filename,
+            Body: file
+        }
+
+        let deploymentParams = {
+            Command: {
+                Name: 'deploy'
+            },
+            StackId: awsOpsworksStackId,
+            AppId: awsOpsworksAppId,
+            Comment: message
+        }
+
+        s3.upload(s3params, function (err, data) {
+            if (err) {
+                core.setFailed(err.toString())
+                throw err
+            }
+            console.log(`File uploaded successfully to S3. ${data.Location}`)
+
+            opsworks.createDeployment(deploymentParams, function (err, data) {
+                if (err) {
+                    core.setFailed(err.toString())
+                    throw err
+                }
+                console.log(`OpsWorks App successfully deployed. ${data.DeploymentId}`)
+            })
+        })
+    }
 }
 
 async function runAction() {
@@ -98,17 +163,14 @@ async function runAction() {
             })
         }
 
-
-
         app = await getAwsOpsworksApp()
 
         if (typeof (app) !== 'undefined') {
 
-            awsOpsworksAppId = app.AppId
             console.log(`OpsWorks App allready setup. ${app.AppId}`)
 
             let appParams = {
-                AppId: awsOpsworksAppId,
+                AppId: app.AppId,
                 Name: wfWebname,
                 StackId: awsOpsworksStackId,
                 Type: 'other',
@@ -127,8 +189,7 @@ async function runAction() {
                     throw err
                 }
                 console.log(`OpsWorks App successfully updated. ${data.AppId}`)
-
-                awsOpsworksAppId = data.AppId
+                deployApp(data.AppId)
             })
 
         } else {
@@ -153,76 +214,8 @@ async function runAction() {
                     throw err
                 }
                 console.log(`New OpsWorks App successfully created. ${data.AppId}`)
-
-                awsOpsworksAppId = data.AppId
+                deployApp(data.AppId)
             })
-        }
-
-        if (awsOpsworksAppId) {
-
-            let githubRef = github.context.ref
-            let commitSha = github.context.sha
-            let eventPayload = github.context.payload
-            let message = ''
-            if (typeof (eventPayload.release) !== 'undefined') {
-                message = 'Release: ' + eventPayload.release.name + ' - "' + eventPayload.release.body + '" '
-            } else if (typeof (eventPayload.commits) !== 'undefined') {
-                message = 'Commit: "' + eventPayload.commits[0].message + '" '
-            }
-            message = message + 'Ref: ' + githubRef + ' (' + commitSha.substring(0, 7) + ')'
-            console.log(execSync('composer validate').toString())
-            console.log(execSync(`composer install --prefer-dist --no-progress --no-suggest`).toString())
-            console.log(execSync(`composer update johnpbloch/wordpress wunderfarm/* --with-dependencies`).toString())
-            console.log(execSync(`sudo npm install -g n`).toString())
-            console.log(execSync(`sudo n v10.15`).toString())
-            console.log(execSync(`npm ci`).toString())
-            console.log(execSync(`npm run build`).toString())
-            execSync(`mkdir dist`).toString()
-            execSync(`cp -R vendor dist`).toString()
-            execSync(`cp -R wp dist`).toString()
-            execSync(`cp -R wp-content dist`).toString()
-            execSync(`cp -R .htaccess dist`).toString()
-            execSync(`cp -R index.php dist`).toString()
-            execSync(`cp -R wp-config.* dist`).toString()
-            execSync(`cp -R robots.txt dist 2>/dev/null || :`).toString()
-            execSync(`zip -rq ${wfWebname} ./dist`).toString()
-
-            let filename = wfWebname + '.zip'
-            let file = fs.readFileSync(filename)
-
-            let s3params = {
-                Bucket: awsS3Bucket,
-                Tagging: 'client=' + wfClient + '',
-                Key: awsOpsworksStackId + '/' + wfWebname + '_' + awsOpsworksAppId + '/' + filename,
-                Body: file
-            }
-
-            let deploymentParams = {
-                Command: {
-                    Name: 'deploy'
-                },
-                StackId: awsOpsworksStackId,
-                AppId: awsOpsworksAppId,
-                Comment: message
-            }
-
-            s3.upload(s3params, function (err, data) {
-                if (err) {
-                    core.setFailed(err.toString())
-                    throw err
-                }
-                console.log(`File uploaded successfully to S3. ${data.Location}`)
-
-                opsworks.createDeployment(deploymentParams, function (err, data) {
-                    if (err) {
-                        core.setFailed(err.toString())
-                        throw err
-                    }
-                    console.log(`OpsWorks App successfully deployed. ${data.DeploymentId}`)
-                })
-            })
-        } else {
-            core.setFailed(`Undefined OpsWorks App ID.`)
         }
 
     } catch (e) {
